65a162ad67e666386cfe6c99e7b548c1b16c9790
Internetmedley <remy.db@live.nl>
2023-11-21 23:51:02 +0100

    dingen weggehaald

--- op1-kNN/main.py
+++ op1-kNN/main.py
@@ -12,11 +12,12 @@ from scipy.stats import mode
 #     RH: total sum of precipitation that day (in 0.1 mm); -1 for less than 0.05mm.
 
 
-cv_scores = {}
-def find_best_K(distances, v_data, v_labels, d_labels, labels):
+def find_best_K(distances, v_labels, data, labels):
+    cv_scores = {}
     best_k = 0
+    best_score = 0
     scores = []
-    for k in range(1, int(len(d_labels)/3)):                                            #it is usually not a good idea to go higher than 1/3 of train data for k
+    for k in range(1, int(len(data)/3)):                                            #it is usually not a good idea to go higher than 1/3 of train data for k
         k_closest = []                                                                  #reset closest neighbours
         n_correct = 0
         for d in range(len(distances)):
@@ -26,29 +27,24 @@ def find_best_K(distances, v_data, v_labels, d_labels, labels):
                 for l in labels:
                     frequencies.append(tuple((np.count_nonzero(k_closest == l), l)))
                 frequencies.sort(key=lambda x : x[0], reverse=True)             #sort the elements descending
-                print(frequencies)
-                
-                #print(frequencies[0][0] == frequencies[1][0])
+
                 if(frequencies[0][0] == frequencies[1][0]):                     #if true, there is a conflict meaning the highest frequent neighbours are the same
                     k_closest = k_closest[:-1]                                  #leave out the last neighbour if there is a conflict and then repeat the process
-                    frequencies = []
-                                                                                #because we're sorting beforehand, comparing the first two elements always works
+                    frequencies = []                                            #because we're sorting beforehand, comparing the first two elements always works  
                     continue
-                    
                 else:
                     break
-            
-            most_common_label = frequencies[0][1]
-            
-            print(most_common_label == v_labels[d], "d: ", d, "k :", k) 
 
-            
+            most_common_label = frequencies[0][1]
             if(most_common_label == v_labels[d]):
-                scores.append(1)
-            else:
-                scores.append(0)
-        cv_scores[k] = np.mean(scores)
-    #print("beste k:", best_k, "beste score:", best_score)
+                n_correct += 1
+        if(n_correct > best_score):
+            best_score = n_correct
+            best_k = k
+
+        cv_scores[k] = best_score / len(v_labels)
+    
+    print("beste k:", best_k, "beste score:", best_score)
     best_k = max(cv_scores, key=cv_scores.get)
     best_score = cv_scores[best_k]
     
@@ -56,9 +52,24 @@ def find_best_K(distances, v_data, v_labels, d_labels, labels):
         print(f"k={k}: Mean Cross validation score = {score}")
 
     print(f"\nBest k value: {best_k} with mean cross validation score = {best_score}")
-
-
     return k
+
+def predict(testData, trainData, k):
+    distances = []
+    for b in range(len(data)):          
+        distance = 0
+        
+        #linalg = np.linalg.norm(validata - data[b], axis=1)
+        # for d in linalg:
+        #     distances.append(tuple((linalg, d_labels[b]))) 
+
+        for i in range(len(data[b])):       
+            distance += np.square(testData[a][i] - trainData[b][i])   
+
+        distances.append(tuple([np.sqrt(distance), d_labels[b]]))                               #sqrt want pythagoras
+
+    distances.sort(key=lambda x : x[0])                 #sort based on first element, which is 
+    allDistancesAndLabels.append(distances)
 
 
 def main():
@@ -82,7 +93,7 @@ def main():
 
     validates = np.genfromtxt('validation1.csv', delimiter=';', usecols=[0])
     v_labels = []
-    for label in dates:
+    for label in validates:
         if label < 20010301:
             v_labels.append('winter')
         elif 20010301 <= label < 20010601:
@@ -118,26 +129,12 @@ def main():
     
     #distances = np.linalg.norm(validata - d, axis=1)
 
-
-    find_best_K(allDistancesAndLabels, data, v_labels, d_labels, labels=["winter", "lente", "zomer", "herfst"])
-
-
-    # Algorithm 3.1 — k-Nearest Neighbours.
-    # Given:
-    # • Training set X of examples (~xi, yi) where
-    # – ~xi is feature vector of example i; and
-    # – yi is class label of example i.
-    # • Feature vector ~x of test point that we want to classify.
-    # Do:
-    # 1. Compute distance D(~x,~xi);
-    # 2. Select k closest instances ~x j1 , . . . , ~x jk with class labels y j1 , . . . , y jk ;
-    # 3. Output class y∗, which is most frequent in y j1 , . . . , y jk 
-
-
-
+    labels=["winter", "lente", "zomer", "herfst"]
+    best_k = find_best_K(allDistancesAndLabels, v_labels, data, labels)
 
     days = np.genfromtxt('days.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
 
+    [ predict(day, data, best_k) for day in days ]
 
 if(__name__ == '__main__'):
     main() 
