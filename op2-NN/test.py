236b6a0035ca2874dcb64acdba3ace087e05ab5f
Internetmedley <remy.db@live.nl>
2024-01-26 22:14:47 +0100

    nomrmalisatie toegevoegd

--- op2-NN/test.py
+++
@@ -1,8 +0,0 @@
-import NN
-import numpy as np
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-
-print(sigmoid(-0.00000007))
--- op6-k-Means/main.py
+++ op6-k-Means/main.py
@@ -55,13 +55,13 @@ def find_most_common_neighbour_K(distancesAndLabels, k):
     return most_common_class
 
 
-def predict(k, y_train, X_train, x_labels):
+def predict(k, distances):
     """
     Returns the prediction for the most common class from datapoints X_train neighbouring y_train given a K value for no. of neighbours.
     """
-    distances = calculate_distances(y_train, X_train, x_labels)
-    most_common_class = find_most_common_neighbour_K(distances, k)
-    return most_common_class
+    most_common_classes = [ find_most_common_neighbour_K(d, k) for d in distances ]
+    return most_common_classes
+
 
 def evaluate(y_predictions, y_labels):
     """
@@ -71,7 +71,7 @@ def evaluate(y_predictions, y_labels):
     accuracy = float(matches / len(y_labels)) * 100
     return accuracy
 
-def find_best_K_val(y_train, y_labels, X_train, x_labels):
+def find_best_K_val(X_distances, y_labels, epochs):
     """
     Returns the best value for K by iterating through a number of them equal to 1/3 of train size and comparing their scores.
 
@@ -86,9 +86,8 @@ def find_best_K_val(y_train, y_labels, X_train, x_labels):
     """ 
     best_k = 0
     best_score = 0
-    epochs = int(len(X_train)/3)                                                #X_train / 3 so it will evaluate k 1 through 121 to see which is best
     for k in range(1, epochs):                                                  
-        predictions = np.array([ predict(k, d, X_train, x_labels) for d in y_train])
+        predictions = np.array(predict(k, X_distances))
         accuracy = evaluate(predictions, y_labels=y_labels)
         print(f"Training... ({k}/{epochs-1}) - k:{k}, accuracy: {accuracy}")
         if( accuracy > best_score ):
@@ -100,43 +99,56 @@ def find_best_K_val(y_train, y_labels, X_train, x_labels):
 
 
 def main():
-    d_data = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+    X_train = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
 
-    dates = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[0])
-    d_labels = []
-    for label in dates:
+    d_dates = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[0])
+    x_labels = []
+    for label in d_dates:
         if label < 20000301:
-            d_labels.append('winter')
+            x_labels.append('winter')
         elif 20000301 <= label < 20000601:
-            d_labels.append('lente')
+            x_labels.append('lente')
         elif 20000601 <= label < 20000901:
-            d_labels.append('zomer')
+            x_labels.append('zomer')
         elif 20000901 <= label < 20001201:
-            d_labels.append('herfst')
+            x_labels.append('herfst')
         else: # from 01-12 to end of year
-            d_labels.append('winter')
+            x_labels.append('winter')
 
-    v_data = np.genfromtxt('validation1.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+    y_train = np.genfromtxt('validation1.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
 
-    validates = np.genfromtxt('validation1.csv', delimiter=';', usecols=[0])
-    v_labels = []
-    for label in validates:
+    v_dates = np.genfromtxt('validation1.csv', delimiter=';', usecols=[0])
+    y_labels = []
+    for label in v_dates:
         if label < 20010301:
-            v_labels.append('winter')
+            y_labels.append('winter')
         elif 20010301 <= label < 20010601:
-            v_labels.append('lente')
+            y_labels.append('lente')
         elif 20010601 <= label < 20010901:
-            v_labels.append('zomer')
+            y_labels.append('zomer')
         elif 20010901 <= label < 20011201:
-            v_labels.append('herfst')
+            y_labels.append('herfst')
         else: # from 01-12 to end of year
-            v_labels.append('winter')
+            y_labels.append('winter')
 
-    k = find_best_K_val(v_data, v_labels, d_data, d_labels)         #this is essentially fitting/training, finding the best no. of neighbours to look at
+    print(X_train.min(axis=0))
+    print(X_train.max(axis=0))
+
+    #X_train = np.array([ (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0)) for X in X_train ])
+    #y_train = np.array([ (y - y.min(axis=0)) / (y.max(axis=0) - y.min(axis=0)) for y in y_train ])
+    
+    X_train = (X_train - X_train.min(axis=0)) / (X_train.max(axis=0) - X_train.min(axis=0))
+    y_train = (y_train - y_train.min(axis=0)) / (y_train.max(axis=0) - y_train.min(axis=0))
+
+    X_distances = [ calculate_distances(y, X_train, x_labels) for y in y_train]
+    epochs = int(len(X_train)/3)
+
+    k = find_best_K_val(X_distances, y_labels, epochs)         #this is essentially fitting/training, finding the best no. of neighbours to look at
                                                                     #given this dataset this will result to: K = 58
 
     days = np.genfromtxt('days.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
-    predictions = np.array([ predict(k, d, d_data, d_labels) for d in days])
+    test_distances = [ calculate_distances(d, X_train, x_labels) for d in days]
+    predictions = predict(k, test_distances)
     print("Voorspellingen: ", predictions)   
 
 if(__name__ == '__main__'):
